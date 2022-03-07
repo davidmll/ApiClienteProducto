84b8c976705a67bb82f25880ab9e32438d6ed069
davidmll <david.martinez-llano@capgemini.com>
2022-03-07 10:24:14 +0100

    feat(master) commit inicial

---
+++ ApiRestCliente-Compra/src/main/java/com/projecto/java/ApiRestClienteCompraApplication.java
@@ -0,0 +1,13 @@
+package com.projecto.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestClienteCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestClienteCompraApplication.class, args);
+	}
+
+}
