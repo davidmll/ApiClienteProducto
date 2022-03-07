ea6cec308c7f98cd2c262a4efaf186b37860f172
davidmll <david.martinez-llano@capgemini.com>
2022-03-07 12:47:55 +0100

    feat(david) vista producto

--- ApiRestCliente-Compra/src/main/java/com/projecto/java/controller/ClienteController.java
+++ ApiRestCliente-Compra/src/main/java/com/projecto/java/controller/ClienteController.java
@@ -1,5 +1,19 @@
 package com.projecto.java.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ClienteController {
+
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/producto")
+	public String producto() {
+		return "producto";
+	}
 
 }
